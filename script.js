5acebaa69cc673b26dab8eae4a6c7595fd353802
Gustavopriori <145362108+Gustavopriori@users.noreply.github.com>
2025-04-22 11:20:33 -0300

    aula2204

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ document.addEventListener('DOMContentLoaded',function () {
         
     })    
   
-  
     const aumentaFonteBotao = document.getElementById ('aumentar.fonte');
     const diminuiFonteBotao = document.getElementById ('diminuir.fonte');
     
@@ -16,14 +15,13 @@ document.addEventListener('DOMContentLoaded',function () {
     
     aumentaFonteBotao.addEventListener('click',function () {
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     
-    });
+    })
     
     
     diminuiFonteBotao.addEventListener('click',function () {
         tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-    });
-    
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+        })
     })
